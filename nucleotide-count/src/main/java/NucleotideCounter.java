77390a7733ee081528ceae567b75df0df9d94ec0
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2024-03-08 08:51:16 +0100

    Basic answert to nucleotide. No stream

--- nucleotide-count/src/main/java/NucleotideCounter.java
+++ nucleotide-count/src/main/java/NucleotideCounter.java
@@ -2,11 +2,42 @@ import java.util.Map;
 import java.util.HashMap;
 
 class NucleotideCounter {
+    private final Map<Character, Integer> nucleotideCounts;
+
     NucleotideCounter(String sequence) {
-        // Put code here
+        if (hasIllegalNucleotides(sequence)) {
+            throw new IllegalArgumentException();
+        }
+
+        nucleotideCounts = countNucleotidesFrequences(sequence);
     }
 
     Map<Character, Integer> nucleotideCounts() {
-        // Put code here
+        return nucleotideCounts;
+    }
+
+    private static Map<Character, Integer> initializeCounts() {
+        Map<Character, Integer> counts = new HashMap<>();
+        
+        for (char nucleotide: new char[] {'A', 'C', 'G', 'T'}) {
+            counts.put(nucleotide, 0);
+        }
+
+        return counts;
+    }
+
+    private static Map<Character, Integer> countNucleotidesFrequences(String sequence) {
+        Map<Character, Integer> nucleotideCounts = initializeCounts();
+        
+        for (char nucleotide: sequence.toCharArray()) {
+            int oldValue = nucleotideCounts.get(nucleotide);
+            nucleotideCounts.replace(nucleotide, oldValue, oldValue + 1);
+        }
+
+        return nucleotideCounts;
+    }
+    
+    private static boolean hasIllegalNucleotides(String sequence) {
+        return !sequence.matches("(A|C|G|T)*");
     }
 }
