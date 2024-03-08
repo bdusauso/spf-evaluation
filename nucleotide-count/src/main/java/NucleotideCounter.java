b819ccec78fbaf8893eff71ad4e2b4b50135033d
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2024-03-08 08:49:04 +0100

    Add empty nucleotide count

---
+++ nucleotide-count/src/main/java/NucleotideCounter.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class NucleotideCounter {
+    NucleotideCounter(String sequence) {
+        // Put code here
+    }
+
+    Map<Character, Integer> nucleotideCounts() {
+        // Put code here
+    }
+}
